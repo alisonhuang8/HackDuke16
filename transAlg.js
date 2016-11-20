93dd06ca54346f6b9682332ab5b7847e769d662f
Jeffery Liu <jeffery300@gmail.com>
2016-11-20 09:21:30 -0500

    transAlg now works

--- transAlg.js
+++ transAlg.js
@@ -9,25 +9,32 @@ CHAR = charity ID number 1-5
 
 function transaction(FP, IDA, cap, DC){
 
-	var X = Math.floor((Math.random()*100)+1);
+	/*ap = document.getElementById("cap").value;*/
+	var X = Math.random()*100+1;
+	console.log(X);
 	var donation;
 
 	if(DC >= cap){
 		FP = FP - X;
 		return 0;
 	} else {
-		donation = ceil(X) - X;
+		donation = Math.ceil(X) - X;
 
-		if(donation > (CAP - DC)){
-			donation = (CAP - DC);
+		if(donation > (cap - DC)){
+			donation = (cap - DC);
 		}
 		FP -= (X + donation);
 		IDA += donation;
 		DC += donation;
-
+		donation = Math.round(donation*100)/100;
+		console.log(donation);
 		return donation;
 
+
 	}
+	console.log(0);
 	return 0; 
 }
-
+var cap = document.getElementById("cap").value;
+console.log(cap)
+transaction(2000,50,cap,0);
