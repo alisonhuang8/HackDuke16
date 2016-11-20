10b529ee789db1f8e61faf1205795de9f9cc96c9
Jeffery Liu <jeffery300@gmail.com>
2016-11-20 04:59:02 -0500

    Wrote php to pull data from the form

---
+++ process.php
@@ -0,0 +1,14 @@
+
+
+
+<?php
+
+$charity = $_POST["charity"];
+$number = $_POST["donationcap"];
+
+echo "The charity you want to contribute to is  $charity , and the max you are willing to donate a day is $donationcap";
+echo $number
+
+?>
+
+
